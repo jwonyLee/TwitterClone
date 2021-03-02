d3aa01265d8a5125a8fd934afdfddc1320a0b9ff
JiWon Lee <jiwon.swdev@gmail.com>
2021-03-02 09:16:06 +0900

    [#1] feat: 컬렉션뷰 기본 구성

--- TwitterClone/TimelineViewController.swift
+++ TwitterClone/TimelineViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class TimelineViewController: UICollectionViewController {
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        collectionView?.backgroundColor = .white
+
+        collectionView.backgroundColor = .white
+
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
     }
-    
-    
+}
+
+extension TimelineViewController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        5
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+
+        cell.backgroundColor = UIColor.red
+
+        return cell
+    }
+}
+
+extension TimelineViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 200)
+    }
 }
