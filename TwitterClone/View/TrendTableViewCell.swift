85e99f803625cd38a4fff5c1e8ff979a704b5e44
JiWon Lee <jiwon.swdev@gmail.com>
2021-03-10 22:17:12 +0900

    [#10] refactor: let으로 선언된 프로퍼티 private lazy var로 변경, TrendTableViewCell

--- TwitterClone/View/TrendTableViewCell.swift
+++ TwitterClone/View/TrendTableViewCell.swift
@@ -11,7 +11,7 @@ class TrendTableViewCell: UITableViewCell {
 
     static let identifier = "trendCell"
 
-    let rankLabel: UILabel = {
+    private lazy var rankLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.adjustsFontForContentSizeCategory = true
@@ -22,7 +22,7 @@ class TrendTableViewCell: UITableViewCell {
         return label
     }()
 
-    let keywordLabel: UILabel = {
+    private lazy var keywordLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.adjustsFontForContentSizeCategory = true
@@ -31,7 +31,7 @@ class TrendTableViewCell: UITableViewCell {
         return label
     }()
 
-    let tweetCountLabel: UILabel = {
+    private lazy var tweetCountLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.adjustsFontForContentSizeCategory = true
