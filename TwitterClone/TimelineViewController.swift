d5e7896bd3652655e1c95280e5f61e333294dc2d
JiWon Lee <jiwon.swdev@gmail.com>
2021-03-10 23:07:27 +0900

    [#10] refactor: extension 분리

--- TwitterClone/TimelineViewController.swift
+++ TwitterClone/TimelineViewController.swift
@@ -34,37 +34,12 @@ class TimelineViewController: UICollectionViewController {
         collectionView.register(TweetCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
     }
 
-    private func configureNavigation() {
-        let button = UIButton(type: .custom)
-        button.setImage(UIImage(systemName: "cloud.bolt.fill"), for: .normal)
-        button.addTarget(self, action: #selector(scrollToTop(_:)), for: .touchUpInside)
-        self.navigationItem.titleView = button
-        self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "list.bullet"),
-                                                                                  style: .plain,
-                                                                                  target: self,
-                                                                                  action: nil)
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "sparkles"),
-                                                                                   style: .plain,
-                                                                                   target: self,
-                                                                                   action: nil)
-    }
-
-    private func configureTweetButton() {
-        view.addSubview(tweetFloatingButton)
-
-        NSLayoutConstraint.activate([
-            tweetFloatingButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
-            tweetFloatingButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10),
-            tweetFloatingButton.widthAnchor.constraint(equalToConstant: 50),
-            tweetFloatingButton.heightAnchor.constraint(equalTo: tweetFloatingButton.widthAnchor)
-        ])
-    }
-
     @objc func scrollToTop(_ sender: UIButton) {
         collectionView.scrollToItem(at: IndexPath(row: 0, section: 0), at: .top, animated: true)
     }
 }
 
+// MARK: - Public: Delegate
 extension TimelineViewController {
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         20
@@ -81,5 +56,34 @@ extension TimelineViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
 
         return CGSize(width: view.frame.width, height: 300)
+    }
+}
+
+// MARK: - Private
+private extension TimelineViewController {
+    func configureNavigation() {
+        let button = UIButton(type: .custom)
+        button.setImage(UIImage(systemName: "cloud.bolt.fill"), for: .normal)
+        button.addTarget(self, action: #selector(scrollToTop(_:)), for: .touchUpInside)
+        self.navigationItem.titleView = button
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "list.bullet"),
+                                                                                  style: .plain,
+                                                                                  target: self,
+                                                                                  action: nil)
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "sparkles"),
+                                                                                   style: .plain,
+                                                                                   target: self,
+                                                                                   action: nil)
+    }
+
+    func configureTweetButton() {
+        view.addSubview(tweetFloatingButton)
+
+        NSLayoutConstraint.activate([
+            tweetFloatingButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -10),
+            tweetFloatingButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10),
+            tweetFloatingButton.widthAnchor.constraint(equalToConstant: 50),
+            tweetFloatingButton.heightAnchor.constraint(equalTo: tweetFloatingButton.widthAnchor)
+        ])
     }
 }
